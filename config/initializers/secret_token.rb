625e7f599d41f7e6e9bfa1ae75500897c53ceeb0
Franco Brusatti <fbrusatti@gmail.com>
2010-10-21 14:44:40 -0300

    Initial commit, skel with devise user configured and basic layout templates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProdeYetero::Application.config.secret_token = '9555941a463da819cbf11cb9f7a6c84673289f6d60397165492080810a6c35d48febbd58ba6c1a59297a12d8b309941d6815fb91634f5f2d8cba161c041678f0'
