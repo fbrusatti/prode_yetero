997437893d4d1937fbe7e9c4f4ee7e1a80ab5dd6
Franco Brusatti <fbrusatti@gmail.com>
2010-11-02 20:32:35 -0300

    start to work with models and layouts togheter

--- app/controllers/admin/leagues_controller.rb
+++ app/controllers/admin/leagues_controller.rb
@@ -4,4 +4,22 @@ class Admin::LeaguesController < Admin::BaseController
     @leagues = League.all
   end
 
+  def new
+    @league = League.new
+  end
+
+  def create
+    @league = League.new(params[:league])
+
+    if @league.save
+      redirect_to admin_leagues_path, :notice => "Successfully created league."
+    else
+      render :action => 'new'
+    end
+  end
+
+  def destroy
+    
+  end
+
 end
